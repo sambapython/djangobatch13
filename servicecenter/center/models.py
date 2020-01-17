b3c90c17c9536f2277b4961092fce91dda4d85fb
sambapython@gmail.com <sambapython@gmail.com>
2020-01-17 08:09:08 +0530

    signin and signup done.

--- servicecenter/center/models.py
+++ servicecenter/center/models.py
@@ -1,15 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import login,logout,authenticate
 
-# Create your models here.
 class NameAbstract(models.Model):
 	# abstract model
 	name = models.CharField(max_length=61)
 	class Meta:
 		abstract = True
 
+class UserProfile(AbstractUser):
+	phone = models.CharField(max_length=20, unique=True, blank=True, null=True)
+	address = models.TextField(blank=True, null=True)
+
+
+
 class UserType(NameAbstract):
 	pass
 	#name = models.CharField(max_length=61)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Create your models here.
+
+
+class Parent(models.Model):
+	name = models.CharField(max_length=60)
+	email = models.EmailField()
+
+class Child(Parent):
+	# one to one relation to parent
+	phone = models.CharField(max_length=25)
+	address = models.TextField(default="address sample")
+
 
 class Product(NameAbstract):
 	#name = models.CharField(max_length=60)
@@ -24,16 +73,5 @@ class SalesOrders(models.Model):
 	description = models.TextField(blank=True, null=True)
 	customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 	products = models.ManyToManyField(Product)
-
-
-
-class Parent(models.Model):
-	name = models.CharField(max_length=60)
-	email = models.EmailField()
-
-class Child(Parent):
-	# one to one relation to parent
-	phone = models.CharField(max_length=25)
-	address = models.TextField(default="address sample")
 
 
--- servicecenter/center/views.py
+++ servicecenter/center/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from center.models import UserProfile
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def home_view(request):
-	return render(request,"center/home.html")
+	msg=""
+	method = request.method.lower()
+	if method=="post":
+		data= request.POST
+		if "signup" in data:
+			user = UserProfile(username=data.get("email"),
+				email=data.get("email"),
+				password=data.get("password"),
+				address = data.get("address"),
+				phone=data.get("phone"))
+			try:
+				user.save()
+				user.set_password(data.get("password"))
+				user.save()
+				msg="user created successfully"
+			except Exception as err:
+				msg = str(err)
+		elif "signin" in data:
+			data = request.POST 
+			username = data.get("email")
+			password = data.get("password")
+			#user = UserProfile.objects.filter(username=username, password=password)
+			user = authenticate(username=username,password=password)
+			if user:
+				msg="authenticated"
+			else:
+				msg="not authenticated"
+			
+	return render(request,"center/home.html",{"message":msg})
